fa2c2e75afd25885693102ca7e9e4b5267b77381
RoS6 <rosayaoyx@gmail.com>
2020-12-02 17:53:49 +0000

    'coloured graph combinations'

--- colourCombination.py
+++ colourCombination.py
@@ -13,15 +13,17 @@ class Edge():
 def checkFailedCombination(rowx,rowy,combination):
     failedCombination = combination.copy()
     for i in range(num):
-        if [rowx[i],rowy[i]] in failedCombination:
-            failedCombination.remove([rowx[i],rowy[i]])
+        if rowx[i] == 0 or rowy[i]==0:
+            continue
+        elif [rowx[i].colour,rowy[i].colour] in failedCombination:
+            failedCombination.remove([rowx[i].colour,rowy[i].colour])
     return failedCombination
 
 def searchForbidden(indexOfRow,indexOfColumn):
     edge = graph[indexOfRow][indexOfColumn]
     row = graph[indexOfRow]
     for col in range(num):
-        if row[col] ==0:
+        if row[col] == 0 or row[col].colour =='n':
             continue
         elif row[col].colour != edge.colour and row[col].colour != 'u':
             graph[indexOfColumn][col].addNotColour(findTheRemainingOne(row[col],edge))
@@ -34,21 +36,79 @@ def findTheRemainingOne(edgex,edgey):
 
 
 def checkValidity(indexX,indexY):
+    global graph,num
     rowx = graph[indexX]
     rowy = graph[indexY]
-    if rowx[indexY].colour == 'u' and rowy[indexX].colour=='u':
+    fail = []
+    combinations = []
+    if rowx[indexY].colour == 'n':
         combinations = notconnected
-        fail = checkFailedCombination(rowx,rowy,combinations)
-        for combi in fail:
-            for col in range(num):
-                if rowx[col].colour == combi[0] and rowy[col].colour == 'u' and rowy[col].not_colour != combi[1]:
-                    rowy[col].colour = combi[1]
-                    searchForbidden(indexX,col)
+    # combinations = (notconnected if (rowx[indexY].colour == 'u' and rowy[indexX].colour=='u') else connected)
+    # fail = checkFailedCombination(rowx,rowy,combinations)
+    elif rowx[indexY].colour == 'u':
+        if len(rowx[indexY].not_colour) ==3:
+            rowx[indexY].colour = 'n'
+            rowy[indexX].colour = 'n'
+            combinations = notconnected
+        else:
+            for i in ['r', 'g', 'b']:
+                if i not in rowx[indexY].not_colour:
+                    rowx[indexY].colour = i
+                    rowy[indexX].colour = i
+                    combinations = connected[i]
+                    searchForbidden(indexX,indexY)
+                    searchForbidden(indexY,indexX)
+    else:
+        combinations = connected[rowx[indexY]]
+    fail = checkFailedCombination(rowx, rowy, combinations)
+    if combinations == []:
+        import sys
+        sys.exit()
+    for combi in fail:
+        for col in range(num):
+            # if rowx[col].colour == combi[0] and rowy[col].colour == 'u' and rowy[col].not_colour != combi[1]:
+            if rowx[col] == 0 or rowx[col].colour == 'n':
+                continue
+            if rowx[col].colour == combi[0] and rowy[col].colour == 'u' and combi[1] not in rowy[col].not_colour:
+                rowy[col].colour = combi[1]
+                graph[col][indexY].colour = combi[1]
+                searchForbidden(indexY,col)
+                searchForbidden(col,indexY)
+                fail.remove(combi)
+            elif rowy[col].colour == combi[1] and rowx[col].colour == 'u' and combi[0] not in rowx[col].not_colour:
+                rowx[col].colour = combi[0]
+                graph[col][indexX].colour = combi[0]
+                searchForbidden(indexX,col)
+                searchForbidden(col,indexX)
+                fail.remove(combi)
+            elif rowx[col].colour == 'u' and rowy[col].colour == 'u' and (combi[0] not in rowx[col].not_colour) and (combi[1] not in rowy[col].not_colour):
+                rowx[col].colour = combi[0]
+                rowy[col].colour = combi[1]
+                graph[col][indexY].colour = combi[1]
+                graph[col][indexX].colour = combi[0]
+                searchForbidden(indexX,col)
+                searchForbidden(indexY,col)
+                searchForbidden(col,indexX)
+                searchForbidden(col,indexY)
 
+    if fail == []:
+        return
+    else:
+        num = num + 1
+        new_row = []
+        for i in graph:
+            # print(i)
+            # print(num)
+            i.append(Edge())
+            new_row.append(Edge())
+        new_row.append(Edge())
+        graph.append(new_row)
+        checkValidity(indexX,indexY)
+        print(num)
 
 
 firstLine = [Edge(),Edge('n'),Edge('r'),Edge(),Edge()]
-graph =[firstLine]+[[Edge(),Edge(),Edge(),Edge(),Edge()]]+\
+graph =[firstLine]+[[Edge('n'),Edge(),Edge(),Edge(),Edge()]]+\
        [[Edge(),Edge(),Edge(),Edge(),Edge()]]+\
        [[Edge(),Edge(),Edge(),Edge(),Edge()]]+\
        [[Edge(),Edge(),Edge(),Edge(),Edge()]]
@@ -72,3 +132,4 @@ while True:
         j = i+1
         while j<num:
             checkValidity(i,j)
+
